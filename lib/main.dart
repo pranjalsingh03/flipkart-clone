c1cbce4d6b9ac2c1d72723b47432d6c841508c0e
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-05-25 18:36:43 +0530

    main dart package

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,17 @@
-import 'package:flutter/material.dart';
-import 'package:project/pages/home.dart';
+import "package:flutter/material.dart";
+import "package:project/pages/home_page.dart";
 
 
-// @pragma(
-//     'vm:entry-point') // Mandatory if the App is obfuscated or using Flutter 3.1+
-// void callbackDispatcher() {
-//   Workmanager().executeTask((task, inputData) {
-//     //simpleTask will be emitted here.
-//     print("Hello world");
-//     return Future.value(true);
-//   });
-// }
+void main() => runApp(new MyApp());
 
-void main() {
-  // Workmanager().initialize(
-  //     callbackDispatcher, // The top level function, aka callbackDispatcher
-  //     isInDebugMode:
-  //         true // If enabled it will post a notification whenever the task is running. Handy for debugging tasks
-  //     );
-  // Workmanager().registerOneOffTask("task-identifier", "simpleTask");
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: home()));
+class MyApp extends StatelessWidget {
+  @override
+  Widget build (BuildContext context) {
+    return new MaterialApp(
+      theme: new ThemeData(
+        primaryColor: Colors.green[400],
+      ),
+      home: new HomePage(),
+    );
+  }
 }
