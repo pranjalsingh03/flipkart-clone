83b268e555f6d500679f4b7760d26f18194e5f2b
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-05-28 21:46:53 +0530

    main dart package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import "package:flutter/material.dart";
 import "package:project/pages/home_page.dart";
-import "package:project/pages/managers_page.dart";
 
 
 void main() => runApp(new MyApp());
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primaryColor: Colors.green[400],
       ),
-      home: new ManagersPage(),
+      home: new HomePage(),
     );
   }
 }
