507f57a09a607deaa159e74a94d2f988dd4474a1
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-05-28 21:30:48 +0530

    string json

---
+++ lib/strings.dart
@@ -0,0 +1,11 @@
+class Strings {
+  static String appTitle = "Dukaandar";
+  static String homeTitle = "Home Page";
+  static String addTitle = "Add Form";
+  static String profileTitle = "Profile Page";
+  static String surveysTitle = "Surveys Page";
+  static String tasksTitle = "Tasks Page";
+  static String storesTitle = "Stores Page";
+  static String managersTitle = "Managers Page";
+  static String statisticsTitle = "Statistics Page";
+}
