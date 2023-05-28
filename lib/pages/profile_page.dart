d4e0bccd30bcd40a373d970778b5cdcfce505911
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-05-28 21:31:14 +0530

    profile page

---
+++ lib/pages/profile_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../strings.dart';
+
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+     return new Center(
+      child: new Text(Strings.profileTitle),
+    );
+  }
+}
