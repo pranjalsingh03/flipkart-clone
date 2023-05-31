a91b48a604430b4ec450813e65016f8f5ac6936f
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-06-01 00:53:32 +0530

    new keyword updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,5 @@
 import "package:flutter/material.dart";
-import "package:project/datamodel/survey.dart";
-import "package:project/pages/add_form.dart";
 import "package:project/pages/home_page.dart";
-import "package:project/pages/login_page.dart";
-import "package:project/pages/manager_form.dart";
-import "package:project/pages/managers_page.dart";
-import "package:project/pages/stores_page.dart";
-import "package:project/pages/survey_form.dart";
-import "package:project/pages/surveys_page.dart";
 
 
 void main() => runApp(new MyApp());
@@ -15,11 +7,11 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build (BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
       // theme: new ThemeData(
       //   primaryColor: Colors.green[400],
       // ),
-      home: new HomePage(),
+      home: HomePage(),
     );
   }
 }
