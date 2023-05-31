336e023fcd211e2ead4c7c91f36840c7203a989a
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-05-31 20:36:10 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
 import "package:flutter/material.dart";
+import "package:project/datamodel/survey.dart";
+import "package:project/pages/add_form.dart";
 import "package:project/pages/home_page.dart";
+import "package:project/pages/login_page.dart";
+import "package:project/pages/manager_form.dart";
+import "package:project/pages/managers_page.dart";
+import "package:project/pages/stores_page.dart";
+import "package:project/pages/survey_form.dart";
+import "package:project/pages/surveys_page.dart";
 
 
 void main() => runApp(new MyApp());
@@ -8,9 +16,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build (BuildContext context) {
     return new MaterialApp(
-      theme: new ThemeData(
-        primaryColor: Colors.green[400],
-      ),
+      // theme: new ThemeData(
+      //   primaryColor: Colors.green[400],
+      // ),
       home: new HomePage(),
     );
   }
