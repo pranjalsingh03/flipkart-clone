446c233871609af2ef8dbc0e2c3c02e3717fe7fd
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-06-06 22:46:40 +0530

    temp

---
+++ lib/pages/tempCodeRunnerFile.dart
@@ -0,0 +1,27 @@
+ // Container(
+            //   padding: EdgeInsets.only(left: 1, top: 20, right: 1),
+            //   child: CarouselSlider(
+            //     options: CarouselOptions(height: 200.0),
+            //     items: [
+            //       "assets/images/kvhlogo.png",
+            //       "assets/images/msgspam.jpg",
+            //       "assets/images/kvhlogo.png"
+            //     ].map((i) {
+            //       return Builder(
+            //         builder: (BuildContext context) {
+            //           return Container(
+            //               margin: EdgeInsets.symmetric(horizontal: 5.0),
+            //               decoration: BoxDecoration(
+            //                   // color: Colors.amber,
+
+            //                   borderRadius: BorderRadius.circular(20)),
+            //               child: Center(
+            //                   child: Image(
+            //                       image: AssetImage(
+            //                 "$i",
+            //               ))));
+            //         },
+            //       );
+            //     }).toList(),
+            //   ),
+            // ),
