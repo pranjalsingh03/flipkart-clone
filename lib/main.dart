fa45651963523ac85f3f43b0743555fdd86aaff1
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-07-08 09:22:16 +0530

    const modifier update

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build (BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: HomePage(),
     );
   }
